9c9d6d66211ade269432caf713f76c1059471d42
Diogo Monte <monte.diogo@gmail.com>
2023-04-23 11:59:15 +0200

    add switch if empty examples

--- src/test/java/com/dom/projectreactor/FluxStringSamplesTest.java
+++ src/test/java/com/dom/projectreactor/FluxStringSamplesTest.java
@@ -4,6 +4,7 @@ import com.dom.projectreactor.basics.flux.FluxSimpleSamples;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
+import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 @RunWith(BlockJUnit4ClassRunner.class)
@@ -22,4 +23,37 @@ public class FluxStringSamplesTest {
 				.expectNext(2, 4, 6)
 				.verifyComplete();
 	}
+
+	@Test
+	public void switchIfEmpty() {
+		final var flux = Flux.just(1)
+				.flatMap(i -> Flux.empty())
+				.switchIfEmpty(Flux.just(2))
+				.map(i -> (int) i + 1);
+
+		final var flux1 = Flux.just(Flux.empty())
+				.switchIfEmpty(Flux.empty())
+				.map(i -> 1);
+
+		final var flux2 = Flux.just(1)
+				.switchIfEmpty(Flux.empty())
+				.map(i -> i + 1);
+
+		StepVerifier.create(flux.concatWith(flux1).concatWith(flux2))
+				.expectNext(3)
+				.expectNext(1)
+				.expectNext(2)
+				.verifyComplete();
+	}
+
+	@Test
+	public void zip() {
+		final var flux = Flux.zip(Flux.just(1), Flux.just(2))
+				.map(tuple -> tuple.getT1() + tuple.getT2());
+
+		StepVerifier.create(flux)
+				.expectNext(3)
+				.verifyComplete();
+	}
+	
 }
